2b767d2a1228d15b2c77e2735308dbf095a7105d
Roman Urbánek <roman.urbanek@qest.cz>
2024-08-01 20:36:29 +0200

    config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By {process.env.CREATOR}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
