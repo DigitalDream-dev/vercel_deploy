388cb65a5a098b2cdb7bf0bbccb0dc0d494817e6
Roman Urbánek <roman.urbanek@qest.cz>
2024-08-16 15:30:02 +0200

    pipeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Hello worl,
+            Hello my world
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
